1120d640c9d41d7d3bdcad38126c0c39d4af9a2d
Sankha-Suvra <sankhasuvhraghatak@gmail.com>
2025-03-22 14:38:56 +0530

    moved to traditional deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
+// import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -13,14 +13,14 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json({limit: "5mb"}));
 app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true,
+    credentials: true
   })
 );
 
